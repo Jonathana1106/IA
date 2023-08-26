0752edc63ee3999b125085d43381ac1c61dab7c5
Jonathana1106 <jonathana1196@gmail.com>
2023-08-26 01:03:23 -0600

    [UPDATE] logic of the states and game

--- TC2-Busquedas/min_max.py
+++ TC2-Busquedas/min_max.py
@@ -12,21 +12,21 @@ PC = +1
 HUMAN = -1
 
 
-def evaluate():
-    score = 0
-    score = score + eval_line(0, 0, 0, 1, 0, 2) # row 0
-    score = score + eval_line(1, 0, 1, 1, 1, 2) # row 1
-    score = score + eval_line(2, 0, 2, 1, 2, 2) # row 2
-    score = score + eval_line(0, 0, 1, 0, 2, 0) # col 0
-    score = score + eval_line(0, 1, 1, 1, 2, 1) # col 1
-    score = score + eval_line(0, 2, 1, 2, 2, 2) # col 2
-    score = score + eval_line(0, 0, 1, 1, 2, 2) # diag
-    score = score + eval_line(0, 2, 1, 1, 2, 0) # inv diag
+def evaluate(state):
+    """
+    """
+    if (gameover(state, PC)):
+        score = +1
+    elif (gameover(state, HUMAN)):
+        score = -1
+    else:
+        score = 0
     return score
 
 def eval_line(r1, c1, r2, c2, r3, c3):
+    """
+    """
     score = 0
-
     '''
     First cell case
     Lets supose X is PC and O is a Human
@@ -35,9 +35,9 @@ def eval_line(r1, c1, r2, c2, r3, c3):
     O ? ?; score = -1
     ? ? ?; score = 0
     '''
-    if PC == board[r1][c1]:
+    if (PC == board[r1][c1]):
         score = +1
-    elif HUMAN == board[r1][c1]:
+    elif (HUMAN == board[r1][c1]):
         score == -1
 
     '''
@@ -48,17 +48,17 @@ def eval_line(r1, c1, r2, c2, r3, c3):
     O O ?; score = -10 because it has more chances to win
     ? ? ?; score = 0
     '''
-    if PC == board[r2][c2]:
-        if score == 1:
+    if (PC == board[r2][c2]):
+        if (score == 1):
             score = +10     # X X ?; it has chances to win
-        elif score == -1:
+        elif (score == -1):
             return 0        # X O ?; it has no chances to win
         else:
             score = +1      # ? X ?; cell 1 is empty
-    elif HUMAN == board[r2][c2]:
-        if score == -1:
+    elif (HUMAN == board[r2][c2]):
+        if (score == -1):
             score = -10     # O O ?; it has chances to win
-        elif score == 1:
+        elif (score == 1):
             return 0        # X O ?; draw
         else:
             score = -1      # ? O ?; cell 1 is empty
@@ -71,17 +71,17 @@ def eval_line(r1, c1, r2, c2, r3, c3):
     O O O; score = -100 lose
     ? ? ?; score = 0
     '''
-    if PC == board[r3][c3]:
-        if score > 0:
+    if (PC == board[r3][c3]):
+        if (score > 0):
             score *=10      # X X X (+100) or ? X X (+10)
-        elif score < 0:
+        elif (score < 0):
             return 0        # X X O or O ? X; draw
         else:
             score = +1      # ? ? X; cell 2 was empty
-    elif HUMAN == board[r3][c3]:
-        if score < 0:
+    elif (HUMAN == board[r3][c3]):
+        if (score < 0):
             score *=10      # O O O (-100) or ? O O (-10)
-        elif score > 1:
+        elif (score > 1):
             return 0        # X X O or X ? O; draw
         else:
             score = -1      # ? ? O; cell 2 was empty
@@ -89,24 +89,38 @@ def eval_line(r1, c1, r2, c2, r3, c3):
     return score
 
 def gameover(state, player):
-    if(
-        state[0][0] == player and state[0][1] == player and state[0][2] == player or
-        state[1][0] == player and state[1][1] == player and state[1][2] == player or
-        state[2][0] == player and state[2][1] == player and state[2][2] == player or
-        state[0][0] == player and state[1][0] == player and state[2][0] == player or
-        state[0][1] == player and state[1][1] == player and state[2][1] == player or
-        state[0][2] == player and state[1][2] == player and state[2][2] == player or
-        state[0][0] == player and state[1][1] == player and state[2][2] == player or
-        state[0][0] == player and state[1][1] == player and state[2][2] == player or
-        state[2][0] == player and state[1][1] == player and state[0][2] == player ):
+    """
+    """
+    winstate = [
+        [state[0][0], state[0][1], state[0][2]],
+        [state[1][0], state[1][1], state[1][2]],
+        [state[2][0], state[2][1], state[2][2]],
+        [state[0][0], state[1][0], state[2][0]],
+        [state[0][1], state[1][1], state[2][1]],
+        [state[0][2], state[1][2], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[0][0], state[1][1], state[2][2]],
+        [state[2][0], state[1][1], state[0][2]]
+    ]
+    if([player, player, player] in winstate):
         return True
     else:
         return False
     
+def clear_cells(state):
+    """
+    """
+    cells = []
+    for i, row in enumerate(state):
+        for j, cell in enumerate(row):
+            if (cell == 0):
+                cells.append([i, j])
+    return cells
+    
 def minimax(state, depth, player):
     if depth == 0 or gameover(state, player):
-        score = evaluate()
-        return score
+        score = evaluate(state)
+        return [-1, -1, score]
 
 # Minimal test
 # print('Results', evaluate())
